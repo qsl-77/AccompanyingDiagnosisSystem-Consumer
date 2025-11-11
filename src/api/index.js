9aed5498d20a8767b4e422d0975ff6abb646e9e5
qsl-77 <3156909270@qq.com>
2025-11-11 17:56:07 +0800

    登陆页面和首页页面显示以及axios二次封装和登录接口联调

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request';
+
+export default {
+    login(data) {
+        return request.post('/login',data)
+    },
+    index() {
+        return request.get('/Index/index')
+    }
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import api from './api'
+
 
 const app = createApp(App)
+// 在实例上绑定属性
+app.config.globalProperties.$api = api
 
 app.use(router)
 
